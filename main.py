4520b1e5638a2dabb20613be4fdd243d1434b3c2
pauliusmui <95112960+pauliusmui@users.noreply.github.com>
2025-02-04 05:49:38 +0000

    testing note API

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # from patient0 import create_patient0
-# payload = create_patient0("David2", "Monday2", 32, "F", "female")
+# payload = create_patient0("Paulius", "Thursday", 37, "M", "male")
 # print(payload)
-from appointment import create_appointment
-payload = create_appointment("booked", "quickchat")
+from note import create_note
+payload = create_note()
 print(payload)
+
+
+# from appointment import create_appointment
+# payload = create_appointment("booked", "quickchat")
+# print(payload)
+
---
+++ note.py
@@ -0,0 +1,26 @@
+import os
+import requests
+import json
+
+XPC_API_KEY = os.environ.get("XPC_API_KEY")
+
+XPC_FHIR_API_BASE_URL = os.environ.get("XPC_FHIR_API_BASE_URL")
+url = XPC_FHIR_API_BASE_URL.rstrip('/') + '/core/api/notes/v1/Note'
+
+headers = {
+    'Authorization': f'Bearer {XPC_API_KEY}',
+    'Content-Type': 'application/json'
+}
+
+
+def create_note():
+  payload = json.dumps({
+      "title": "Some Custom Title",
+      "noteTypeName": "Office visit",
+      "patientKey": "b9d03a4edc0348caa6c030a259caa4ce",
+      "providerKey": "9b31de0f2040478e94e4f9b9b409bce0",
+      "practiceLocationKey": "d1eacdb5-9ead-47ce-855a-c8c6ef3932a6",
+      "encounterStartTime": "2025-02-03T19:00:00.016852Z"
+  })
+
+  return requests.request("POST", url, headers=headers, data=payload)
